87b50e5e157fecbbb31709834a6af57ef781afe7
Cris0Smelted <cristhianstavross@gmail.com>
2026-04-01 21:37:56 -0300

    37th time.

---
+++ README.md
@@ -0,0 +1,5 @@
+# DiscordBotofGreenpath
+A Casual bot of discord that focuses on the enviroment.
+It is a points based discord bot that uses quick challenges to the player related to
+the climate change or enviromental issues, it helps users that are unaware of this planetary issue to
+learn about it making it quick and interesting.
---
+++ mbot.py
@@ -0,0 +1,85 @@
+import discord
+import os 
+from discord.ext import commands
+from data import CHALLENGES
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='/', intents=intents)
+users = {
+
+}
+
+challenges1 = list(CHALLENGES.keys())
+
+def user_create(userid, name):
+    if userid not in users:
+        users[userid] = {
+            "name" : name,
+            "points" : 0,
+            "level" : 1,
+            "ac_challenge" : 0
+        } 
+
+def levelcalc(points):
+    if points == 19:
+        return 4
+    elif points >= 12:
+        return 3
+    elif points >= 7:
+        return 2
+    else:
+        return 1
+
+def uptlevel(userid):
+    user_point = users[userid]["points"]
+    levels = levelcalc(user_point)
+    users[userid]["level"] = levels
+
+def actualchallenge(userid):
+    act = users[userid]["ac_challenge"]
+    if act < len(challenges1):
+        key_chall = challenges1[act]
+        return key_chall, CHALLENGES[key_chall]
+    else:
+        return None, None
+
+def challengeup(userid):
+    users[userid]["ac_challenge"] += 1
+
+# def levelz(userid):
+    # if CHALLENGES []:
+    
+@bot.event
+async def on_ready():
+    print(f"a sus ordenes mi general")
+
+@bot.command()
+async def profile(ctx):
+    user_id = str(ctx.author.id)
+    names = ctx.author.name
+    user_create(user_id, names)
+    user = users[user_id]
+    await ctx.send(f" Profile of {user["name"]}\n Points : {user["points"]}\n Levels : {user["level"]}")
+
+@bot.command()
+async def hi(ctx):
+    with open("environmental-issues1.png", "rb") as file:
+        image = discord.File(file)
+    await ctx.send(f"Hello!, I am Greenpath, a bot related to Enviromental issues and climate change.\n ")
+    await ctx.send(file = image)
+
+
+@bot.command()
+async def challenge(ctx):
+    userid = str(ctx.author.id)
+    name = ctx.author.name
+    user_create(userid, name)
+    keychall, act_chall = actualchallenge(userid)
+    if act_chall is None:
+        await ctx.send("Congratulations! You have completed all the challenges in your way!!")
+        return 
+    await ctx.send(f"Challenge: {act_chall["title"]}\n Question: {act_chall["question"]}\n Options: {act_chall["options"]}")
+
+bot.run("")
